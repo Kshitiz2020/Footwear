3beb13b5bf263c972531520988eea8ff47b47ea9
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-11-23 09:35:56 +0200

    hero section accomplish

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,34 +1,41 @@
+import { useState } from "react";
+
+import { shoes, statistics } from "../constants/constants";
 import Button from "../components/Button";
 import ShoeCard from "../components/ShoeCard";
-import { arrowRight } from "../assets/icons";
-import { shoes, statistics } from "../constants/constants";
 import { bigShoe1 } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
-function Hero() {
+const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
-      className="flex flex-col justify-center w-full gap-10 xl:flex-row main-h-screen max-container"
+      className="flex flex-col justify-center w-full min-h-screen gap-10 xl:flex-row max-container"
     >
-      <div className="relative flex flex-col items-start justify-center w-full xl:w-2/5 max:xl:padding-x pt-28">
+      <div className="relative flex flex-col items-start justify-center w-full xl:w-2/5 max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px">
-          <span className="relative z-10 pr-10 Xl:bg-white xl:whitespace-nowrap">
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="relative z-10 pr-10 xl:bg-white xl:whitespace-nowrap">
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red">Nike</span> Shoes
+          <span className="inline-block mt-3 text-coral-red">Nike</span> Shoes
         </h1>
         <p className="mt-6 text-lg leading-8 font-montserrat text-slate-gray mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals,quality comfort , and innovation for
+          Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
-        <Button label="ShopNow" iconURL={arrowRight} />
+
+        <Button label="Shop now" iconURL={arrowRight} />
+
         <div className="flex flex-wrap items-start justify-start w-full gap-16 mt-20">
           {statistics.map((stat, index) => (
-            <div key={stat.label}>
+            <div key={index}>
               <p className="text-4xl font-bold font-palanquin">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
@@ -38,22 +45,23 @@ function Hero() {
         </div>
       </div>
 
-      {/*    for image */}
-      <div className="relative flex justify-center flex-1 bg-center item-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero">
+      <div className="relative flex items-center justify-center flex-1 bg-center bg-cover xl:min-h-screen max-xl:py-40 bg-primary bg-hero">
         <img
-          src={bigShoe1}
-          alt="bigShoeImage"
+          src={bigShoeImg}
+          alt="shoe colletion"
           width={610}
-          height={500}
+          height={502}
           className="relative z-10 object-contain"
         />
-        <div>
-          {shoes.map((shoe, index) => (
-            <div key={shoe}>
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((image, index) => (
+            <div key={index}>
               <ShoeCard
-                imgURL={shoe}
-                changeBigShoeImage={() => {}}
-                bigShoeImg=""
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
@@ -61,5 +69,6 @@ function Hero() {
       </div>
     </section>
   );
-}
+};
+
 export default Hero;
